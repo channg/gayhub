302f234aebd43e09f91e5fb3e96538e925b7442f
channg <xssql@foxmail.com>
2019-04-04 11:52:01 +0800

    fix some add add readme.md

--- index.js
+++ index.js
@@ -9,10 +9,29 @@ program
     .option('-l ,--level <items>', 'level')
     .option('-m ,--multiplier <items>', 'multiplier')
     .action(function (x, b, c) {
-      let dataStart = x
-      let dataend = b
-      let level = c['level']
-      let multiplier = c['multiplier']
+      let dataStart = x;
+      let dataend = b;
+      if (dataStart) {
+        if (dataStart.length !== 8) {
+          console.log('\u001b[31mWrong time format, correct format YYYYMMDD!\u001b[39m')
+          return;
+        }
+      }
+      if (dataend) {
+        if (dataend.length !== 8) {
+          console.log('\u001b[31mWrong time format, correct format YYYYMMDD!\u001b[39m')
+          return;
+        }
+      }
+      let level = parseInt(c['level']) ? parseInt(c['level']) : 4;
+      let multiplier = parseInt(c['multiplier']) ? parseInt(c['multiplier']) : 3;
+      if (level > 10||level<0) {
+        console.log('\u001b[31mLevel ranges from 0 to 9 \u001b[39m')
+        return;
+      }
+      if (multiplier < 0) {
+        multiplier = 1
+      }
       gayhub(dataStart, dataend, level, multiplier)
     })
     .parse(process.argv)
---
+++ readme.md
@@ -0,0 +1,42 @@
+# gayhub
+
+>  Make a nice Contribution wall👿👿👿👿
+
+###  try it
+* Enter your own github repository
+```
+	cd some_repository
+```
+* Run the command
+```
+	npm i -g gayhub
+	gayhub
+```
+or
+```
+	npx gayhub
+```
+###  And then you're going to have a nice Contribution wall🎉🎉🎉🎉🎉
+
+![](https://ws3.sinaimg.cn/large/005BYqpggy1g1qfs0mapuj30x4074gnk.jpg)
+
+
+### api
+* startDate
+```
+	gayhub 20121229
+```
+* endDate
+```
+	gayhub 20180220 20180801
+```
+* level( 0-9) default is 4
+> the level is mean  Random frequency,The smaller the number, the more stable it is.
+```
+	gayhub 20180220 20180801 -l 4
+```
+* multiplier  default is 3
+> Number of repetitions per day
+```
+	gayhub 20180220 20180801 -l 4 -m 3
+```
